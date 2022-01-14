e52d66f08e4686440e470a05eb633848a046d776
Borisov Andrey <80087707+borisovandru@users.noreply.github.com>
2022-01-14 22:40:26 +0400

    Практическая работа по методичке

---
+++ app/src/androidTest/java/com/android/espressotest/MainActivityEspressoTest.kt
@@ -0,0 +1,58 @@
+package com.android.espressotest
+
+import android.view.View
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.android.espressotest.view.search.MainActivity
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityEspressoTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+
+    @Test
+    fun activitySearch_IsWorking() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+
+        if (BuildConfig.TYPE == MainActivity.FAKE) {
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
+        } else {
+            onView(isRoot()).perform(delay())
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2657")))
+        }
+    }
+
+    private fun delay(): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $4 seconds"
+            override fun perform(uiController: UiController, v: View?) {
+                uiController.loopMainThreadForAtLeast(4000)
+            }
+        }
+    }
+
+}
